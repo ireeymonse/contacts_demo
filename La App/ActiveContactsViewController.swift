11ca3036a92227d02e182348839f59ea5d596706
MacBook Pro <mbp@macbook-pro.local>
2018-08-07 18:02:14 -0500

    Basic app structure

---
+++ La App/ActiveContactsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ActiveContactsViewController.swift
+//  La App
+//
+//  Created by MacBook Pro on 8/7/18.
+//  Copyright © 2018 Iree García. All rights reserved.
+//
+
+import UIKit
+
+class ActiveContactsViewController: UIViewController {
+   
+   override func viewDidLoad() {
+      super.viewDidLoad()
+   }
+   
+   override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+   }
+   
+   @IBAction func unwindToHome(_ segue: UIStoryboardSegue) {}
+}
